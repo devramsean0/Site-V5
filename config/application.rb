d8c57a1392bba4a2e0b74175eeb88f933d14c631
Sean Outram <me@sean.cyou>
2025-05-30 16:48:10 +0000

    Start laout of site

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,5 @@ module SiteV5
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Exclude the 7.css docs from css bundling
-    config.assets.precompile.reject! { |path| path.start_with?('7.css/docs/') }
   end
 end
